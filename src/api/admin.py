cdcb7a90ace9a6d00ba6946f3202c79c602e76d2
Brice Venayre <brice.venayre@gmail.com>
2022-10-22 17:16:17 +0200

    add admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from api.models import Projects, Issues, Contributors, Comments, CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """CustomUser class."""
+    pass
+
+
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "type", "author_user_id")
+    autocomplete_fields = ("author_user_id",)
+
+
+@admin.register(Issues)
+class IssuesAdmin(admin.ModelAdmin):
+
+    list_display = ("title", "tag", "priority", "status", "project_id", "created_time")
+
+
+@admin.register(Contributors)
+class ContributorsAdmin(admin.ModelAdmin):
+
+    list_display = ("project_id", "user_id", "permission", "role")
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+
+    list_display = ("author_user_id", "issue_id", "created_time")
+
+# class ArticleAdmin(admin.ModelAdmin):
+#
+#     list_display = ('name', 'product', 'category', 'active')
+#
+#     @admin.display(description='Category')
+#     def category(self, obj):
+#         return obj.product.category
--- src/api/models.py
+++ src/api/models.py
@@ -11,19 +11,29 @@ class CustomUser(AbstractUser):
 
 class Projects(models.Model):
 
+    class Meta:
+        verbose_name = 'Project'
+
     class Type(models.TextChoices):
         BACK_END = 'BE'
         FRONT_END = 'FE'
         IOS = 'IOS'
         ANDROID = "AN"
 
-    title = models.fields.CharField(max_length=128, verbose_name="Titre")
-    description = models.fields.CharField(max_length=2048, blank=True, verbose_name="Description")
-    type = models.fields.CharField(max_length=5, choices=Type.choices, verbose_name="Type")
-    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    title = models.fields.CharField(max_length=128)
+    description = models.fields.CharField(max_length=2048, blank=True)
+    type = models.fields.CharField(max_length=5, choices=Type.choices)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
+                                       verbose_name="Author")
+
+    def __str__(self):
+        return f"{self.title}"
 
 
 class Issues(models.Model):
+
+    class Meta:
+        verbose_name = 'Issue'
 
     class Tag(models.TextChoices):
         BUG = 'BUG'
@@ -40,28 +50,40 @@ class Issues(models.Model):
         IN_PROGRESS = 'IP'
         COMPLETED = 'COMP'
 
-    title = models.fields.CharField(max_length=128, verbose_name="Titre")
-    description = models.fields.CharField(max_length=2048, blank=True, verbose_name="Description")
-    tag = models.fields.CharField(max_length=5, choices=Tag.choices, verbose_name="Tag")
-    priority = models.CharField(max_length=128, choices=Priority.choices, verbose_name="Priorité")
-    project_id = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
+    title = models.fields.CharField(max_length=128)
+    description = models.fields.CharField(max_length=2048, blank=True)
+    tag = models.fields.CharField(max_length=5, choices=Tag.choices)
+    priority = models.CharField(max_length=128, choices=Priority.choices)
+    project_id = models.ForeignKey(to=Projects, on_delete=models.CASCADE, verbose_name="Project")
     status = models.CharField(max_length=128, choices=Status.choices)
     author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
-                                       related_name="author_issues")
+                                       related_name="author_issues", verbose_name="Author")
     assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
-                                         related_name="assignee_issues")
+                                         related_name="assignee_issues", verbose_name="Assignee")
     created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
 
 
 class Contributors(models.Model):
-    project_id = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Contributor'
+
+    project_id = models.ForeignKey(to=Projects, on_delete=models.CASCADE, verbose_name="Project")
+    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="User")
     permission = models.CharField(max_length=128)
     role = models.CharField(max_length=128)
 
 
 class Comments(models.Model):
+
+    class Meta:
+        verbose_name = 'Comment'
+
     description = models.fields.CharField(max_length=2048, verbose_name="Description")
-    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    issue_id = models.ForeignKey(to=Issues, on_delete=models.CASCADE)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
+                                       verbose_name="Author")
+    issue_id = models.ForeignKey(to=Issues, on_delete=models.CASCADE, verbose_name="Issue")
     created_time = models.DateTimeField(auto_now_add=True)
