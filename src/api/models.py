1f6c3e2122a0271c00967e978f75c5dc83c738b6
Brice Venayre <brice.venayre@gmail.com>
2022-10-22 15:59:00 +0200

    add models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,67 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from issuetracking import settings
+
+
+class CustomUser(AbstractUser):
+    """Class User."""
+    pass
+
+
+class Projects(models.Model):
+
+    class Type(models.TextChoices):
+        BACK_END = 'BE'
+        FRONT_END = 'FE'
+        IOS = 'IOS'
+        ANDROID = "AN"
+
+    title = models.fields.CharField(max_length=128, verbose_name="Titre")
+    description = models.fields.CharField(max_length=2048, blank=True, verbose_name="Description")
+    type = models.fields.CharField(max_length=5, choices=Type.choices, verbose_name="Type")
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+
+class Issues(models.Model):
+
+    class Tag(models.TextChoices):
+        BUG = 'BUG'
+        IMPROVEMENT = 'IMP'
+        TASK = 'TASK'
+
+    class Priority(models.TextChoices):
+        LOW = 'LOW'
+        MEDIUM = 'MED'
+        HIGH = 'HIGH'
+
+    class Status(models.TextChoices):
+        TO_DO = 'TD'
+        IN_PROGRESS = 'IP'
+        COMPLETED = 'COMP'
+
+    title = models.fields.CharField(max_length=128, verbose_name="Titre")
+    description = models.fields.CharField(max_length=2048, blank=True, verbose_name="Description")
+    tag = models.fields.CharField(max_length=5, choices=Tag.choices, verbose_name="Tag")
+    priority = models.CharField(max_length=128, choices=Priority.choices, verbose_name="Priorité")
+    project_id = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
+    status = models.CharField(max_length=128, choices=Status.choices)
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
+                                       related_name="author_issues")
+    assignee_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True,
+                                         related_name="assignee_issues")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Contributors(models.Model):
+    project_id = models.ForeignKey(to=Projects, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    permission = models.CharField(max_length=128)
+    role = models.CharField(max_length=128)
+
+
+class Comments(models.Model):
+    description = models.fields.CharField(max_length=2048, verbose_name="Description")
+    author_user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    issue_id = models.ForeignKey(to=Issues, on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
--- src/issuetracking/settings.py
+++ src/issuetracking/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # My apps
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'api.CustomUser'
