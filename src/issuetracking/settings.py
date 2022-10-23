31eadfdc0aeca1eb6cedd6c5c2d3cae332fe4a6d
Brice Venayre <brice.venayre@gmail.com>
2022-10-24 01:18:40 +0200

    add pagination

--- src/issuetracking/settings.py
+++ src/issuetracking/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'api.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+}
